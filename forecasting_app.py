d9cde636be78d4d20c553562b23707d8cb66a199
Renzo <rbenavides_08@hotmail.com>
2022-01-30 13:28:38 -0800

    Add ACF PACF plots

--- forecasting_app.py
+++ forecasting_app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 from models import *
-from plots import plot_forecast
+from plots import *
 
 input_data = None
 
@@ -23,33 +23,45 @@ input_data = st.sidebar.file_uploader("", type=[".xlsx", ".csv"])
 
 if input_data:
     input_df = reading_dataset()
+
+    ## general inputs
     start_date = st.slider(
         "When to do the prediction?",
         value=input_df.index[-1].to_pydatetime(),
-        format="MM/DD/YY -hh:mm",
+        format="YY/MM/DD hh:mm",
         min_value=input_df.index[0].to_pydatetime(),
+        max_value=input_df.index[-1].to_pydatetime(),
     )
-
-    df = input_df.loc[input_df.index <= start_date]
-
-    col = st.sidebar.selectbox("Select a column", df.columns)
+    col = st.sidebar.selectbox("Select a column", input_df.columns)
     model_name = st.sidebar.selectbox("Select a model", ["ARIMA"])
     freq = st.sidebar.text_input("Enter frequency as string e.g. 1D", value="1D")
-    horizon = st.sidebar.text_input("Enter horizon as string e.g. 1D", value="1D")
+    horizon = st.sidebar.number_input("Enter horizon as integer", value=1)
     p = st.sidebar.number_input("Auto Regressive(p)", value=1)
     d = st.sidebar.number_input("Difference (d)", value=0)
+    lags = st.sidebar.number_input("Lags", value=1)
+
+    horizon = str(horizon * int(freq[:-1])) + freq[-1]
+
+    ts = input_df[col].dropna().resample(freq).mean()
+
+    historical = ts.loc[ts.index <= start_date]
+    actual = ts.loc[
+        (ts.index > start_date) & (ts.index <= start_date + pd.to_timedelta(horizon))
+    ]
 
     if model_name == "ARIMA":
-
-        ts = df[col].dropna().resample(freq).mean()
         model = ARIMAModel(horizon=horizon, freq=freq, order=(p, d, 0))
-        model.fit(ts)
+        model.fit(historical)
         prediction = model.predict()
     else:
         model_name = None
 
-    fig = plot_forecast(df[col], prediction)
+    fig = plot_forecast(historical=historical, actual=actual, prediction=prediction)
     st.plotly_chart(fig)
+
+    st.subheader("ACF, PACF Plots")
+    fig = auto_corr_plot(ts=historical, lags=lags)
+    st.pyplot(fig)
 
 else:
     st.text("To use this tool, just upload a csv file with tabular format")
--- plots.py
+++ plots.py
@@ -1,21 +1,55 @@
+import matplotlib.pyplot as plt
 import plotly.graph_objs as go
+import statsmodels as sm
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 
-def plot_forecast(ts, prediction, title=None, xlabel=None, ylabel="value"):
+def plot_forecast(
+    historical, actual, prediction, title=None, xlabel=None, ylabel="value"
+):
 
-    data =[]
-    data.append(go.Scatter(
-            name='Actual',
-            x=ts.index.values,
-            y=ts.values,
-            mode='markers'
-        ))
-    data.append(go.Scatter(
-        name='Predicted',
-        x=prediction.index.values,
-        y=prediction.values,
-        mode='markers'
-    ))
+    data = []
+    data.append(
+        go.Scatter(
+            name="Historical",
+            x=historical.index.values,
+            y=historical.values,
+            mode="lines+markers",
+        )
+    )
+    data.append(
+        go.Scatter(
+            name="Actual", x=actual.index.values, y=actual.values, mode="lines+markers"
+        )
+    )
+    data.append(
+        go.Scatter(
+            name="Predicted",
+            x=prediction.index.values,
+            y=prediction.values,
+            mode="lines+markers",
+        )
+    )
     fig = go.Figure(data=data)
-    
+
+    return fig
+
+
+def auto_corr_plot(ts, lags=None, figsize=(12, 7)):
+    """
+    Plots time series, auto correlation, partial auto correlation, and performs AD Fuller test
+    """
+    fig = plt.figure(figsize=figsize)
+    layout = (2, 2)
+    ts_vals = plt.subplot2grid(layout, (0, 0), colspan=2)
+    acf_vals = plt.subplot2grid(layout, (1, 0))
+    pacf_vals = plt.subplot2grid(layout, (1, 1))
+
+    ts.plot(ax=ts_vals)
+    plot_acf(ts, lags=lags, ax=acf_vals)
+    plot_pacf(ts, lags=lags, ax=pacf_vals)
+    plt.tight_layout()
+
+    p_value = sm.tsa.stattools.adfuller(ts)[1]
+    print(f"Dickey Fuller Test: {p_value:0.5f}")
     return fig
